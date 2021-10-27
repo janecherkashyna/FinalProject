7a643d20a6f891ac21055758bbdedec97b655b51
janecherkashyna <marishka200410@gmail.com>
2021-10-27 21:14:08 +0300

    Refactoring files+reconnecting to db

---
+++ src/getengine.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine
+
+
+engine = create_engine(f'postgresql+psycopg2://umrckcrskukywu:9a1316ab20ed67d3e1f5ab92a4ace174922bc810'
+                       f'3195db1049e4e71de1e6b42e@ec2-52-19-96-181.eu-west-1.compute.amazonaws.com:5432/da9cormpjuqpis')
